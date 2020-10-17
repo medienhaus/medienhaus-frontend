85d10b89d1a19f5b4c857d3432435bd2b993833c
Andi Rueckel <andirueckel@users.noreply.github.com>
2020-10-17 22:49:31 +0200

    add public facing html

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Header from './components/header';
 import Footer from "./components/footer";
 
 // Code-splitting is automated for `routes` directory
-import Landing from "./routes/landing"
-import Dashboard from "./routes/dashboard"
 import Account from "./routes/account";
 import Explore from "./routes/explore";
 import Support from "./routes/support";
@@ -14,13 +12,12 @@ import Login from "./routes/login";
 import Logout from "./routes/logout";
 import Request from "./routes/request_room";
 const App = () => (
-  <>
+  <React.Fragment>
     <AuthProvider>
       <Router>
         <Header />
         <Switch>
-          <Route path="/" exact component={Landing} />
-          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/" />
           <Route path="/account" component={Account} />
           <Route path="/explore" component={Explore} />
           <Route path="/support" component={Support} />
@@ -28,12 +25,44 @@ const App = () => (
           <Route path="/login" component={Login} />
           <Route path="/logout" component={Logout} />
         </Switch>
+        <section id="definition">
+          <blockquote><em>classroom, n.</em> &mdash; shared space to <strong>communicate</strong> and exchange, <strong>meet</strong> face to face, <strong>present</strong> to each other, and <strong>study</strong> together.</blockquote>
+        </section>
+        <section id="introduction">
+          <p><a href="https://www.udk-berlin.de/en/university/college-of-architecture-media-and-design/medienhaus/" rel="nofollow noreferrer" target="_blank">Medienhaus</a> is the media design and art department of Berlin University of the Arts; it is an experimental playground shared amongst Visual Communication, Art and Media, and Communication in Social and Economic Contexts.</p>
+          <p>As the COVID-19 pandemic happened, we quickly needed to provide an intuitive remote collaboration platform, satisfying the requirements and needs of an art school during this crisis and beyond.</p>
+          <p>We rapidly prototyped and iteratively enhanced a privacy-focused, free and open-source set of tools and services for our students and staff, <em>which we soon want to share in form of documentation and automated installation scripts.</em></p>
+        </section>
+        <section id="shoutout">
+          <blockquote>We developed <strong>medienhaus/</strong> &mdash; not replacing but extending the name-giving physical space.</blockquote>
+        </section>
+        <section id="services">
+          <ul>
+            <li><strong>/classroom</strong> asynchronous communication space</li>
+            <li><strong>/meet</strong> audio/video collaboration and presentation</li>
+            <li><strong>/write</strong> collaborative writing, reading, and editing</li>
+            <li><strong>/stream</strong> audio/video live streaming and playback</li>
+          </ul>
+        </section>
+        <section id="team">
+          <h2><strong>Team</strong> (sorted alphabetically):</h2>
+          <ul>
+            <li>Alexej Bormatenkow</li>
+            <li>Dirk Erdmann</li>
+            <li><a href="mailto:kg@udk-berlin.de?subject=medienhaus/" rel="external nofollow noreferrer">Klaus Gasteier</a></li>
+            <li>Marcel Haupt</li>
+            <li><a href="mailto:dh@udk-berlin.de?subject=medienhaus/" rel="external nofollow noreferrer">Daniel Hromada</a></li>
+            <li>Frederik Müller</li>
+            <li>Andi Rueckel</li>
+            <li>Merani Schilcher</li>
+            <li><a href="mailto:rfws@udk-berlin.de?subject=medienhaus/" rel="external nofollow noreferrer">Robert Schnüll</a></li>
+            <li>Paul Seidler</li>
+          </ul>
+        </section>
         <Footer />
       </Router>
     </AuthProvider>
-
-
-  </>
+  </React.Fragment>
 )
 
 export default App;
