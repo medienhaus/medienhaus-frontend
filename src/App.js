c5fc9c60ab3658807df5dfe4f55847086b6099d2
Andi Rueckel <andirueckel@users.noreply.github.com>
2020-10-20 20:58:28 +0200

    completely re-structure html and re-write css grid layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { LocationProvider } from './components/context/LocationContext'
-import Header from './components/header';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { LocationProvider } from "./components/context/LocationContext"
+import { UserProvider } from "./components/context/UserContext";
+import Header from "./components/header";
 import Footer from "./components/footer";
+import Nav from "./components/nav";
+
 // Code-splitting is automated for `routes` directory
 import Account from "./routes/account";
 import Explore from "./routes/explore";
 import Support from "./routes/support";
+import Request from "./routes/request";
+
+import Faq from "./routes/faqs";
+import { Docs } from "./routes/docs";
+
 import Login from "./routes/login";
 import Logout from "./routes/logout";
-import Request from "./routes/request";
-import Landing from './routes/landing';
-import Dashboard from './routes/dashboard';
-import Faq from './routes/help';
-import { Docs } from './routes/docs';
-import { UserProvider } from './components/context/UserContext';
+
+import Landing from "./routes/landing";
+import Dashboard from "./routes/dashboard";
 
 const App = () => (
   <React.Fragment>
@@ -22,18 +27,21 @@ const App = () => (
       <LocationProvider>
         <Router>
           <Header />
-          <Switch>
-            <Route path="/" exact component={Landing} />
-            <Route path='/dashboard' component={Dashboard} />
-            <Route path="/account" component={Account} />
-            <Route path="/explore" component={Explore} />
-            <Route path="/support" component={Support} />
-            <Route path="/request" component={Request} />
-            <Route path="/login" component={Login} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/help" component={Faq} />
-            <Route path="/docs" component={Docs} />
-          </Switch>
+          <main>
+            <Switch>
+              <Route path="/" exact component={Landing} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/account" component={Account} />
+              <Route path="/explore" component={Explore} />
+              <Route path="/support" component={Support} />
+              <Route path="/request" component={Request} />
+              <Route path="/faqs" component={Faq} />
+              <Route path="/docs" component={Docs} />
+              <Route path="/login" component={Login} />
+              <Route path="/logout" component={Logout} />
+            </Switch>
+          </main>
+          <Nav />
           <Footer />
         </Router>
       </LocationProvider>
