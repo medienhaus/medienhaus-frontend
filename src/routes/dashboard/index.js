69ad27066857c244bd6f2d5fe5fe1797a2a870b1
Andi Rueckel <andirueckel@udk-berlin.de>
2020-10-20 14:12:07 +0200

    provisorische sidebar in dashboard

--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -42,17 +42,31 @@ const Dashboard = () => {
   return (
     token === null ? <Redirect to='/' /> : (
       profile.length === 0 ? (<Loading />) : (
-        <section className="landing">
-          <h2>Hello <strong>{profile.displayname}</strong>, you are now logged in.</h2> {user}
-          <ul>
-            <li><strong><a href="/account">/account</a></strong> your profile and a list of your rooms</li>
-            <li><strong><a href="/explore">/explore</a></strong> explore, join, and leave public rooms</li>
-            <li><strong><a href="/docs">/docs</a></strong> documentation, how-to and usage guide</li>
-            <li><strong><a href="/help">/help</a></strong> frequently asked questions and answers</li>
-            <li><strong><a href="/support">/support</a></strong> in case of undocumented problems</li>
-            <li><strong><a href="/request">/request</a></strong> openly accessible public rooms</li>
-          </ul>
-          <Videos />
+        <section className="landing with-sidebar">
+          <div>
+            <div>
+              <ul>
+                <li><strong><a href="/account">/account</a></strong></li>
+                <li><strong><a href="/explore">/explore</a></strong></li>
+                <li><strong><a href="/docs">/docs</a></strong></li>
+                <li><strong><a href="/help">/help</a></strong></li>
+                <li><strong><a href="/support">/support</a></strong></li>
+                <li><strong><a href="/request">/request</a></strong></li>
+              </ul>
+            </div>
+            <div>
+              <h2>Hello <strong>{profile.displayname}</strong>, you are now logged in.</h2> {user}
+              <ul>
+                <li><strong><a href="/account">/account</a></strong> your profile and a list of your rooms</li>
+                <li><strong><a href="/explore">/explore</a></strong> explore, join, and leave public rooms</li>
+                <li><strong><a href="/docs">/docs</a></strong> documentation, how-to and usage guide</li>
+                <li><strong><a href="/help">/help</a></strong> frequently asked questions and answers</li>
+                <li><strong><a href="/support">/support</a></strong> in case of undocumented problems</li>
+                <li><strong><a href="/request">/request</a></strong> openly accessible public rooms</li>
+              </ul>
+              <Videos />
+            </div>
+          </div>
         </section>
       )
     )
