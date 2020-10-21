423ec05765022861e3a212fb9a985fc3cefc5929
Andi Rueckel <andirueckel@users.noreply.github.com>
2020-10-21 17:17:59 +0200

    your local kino now offers a sidebar, too

--- src/routes/kino/index.js
+++ src/routes/kino/index.js
@@ -11,20 +11,46 @@ const Kino = () => {
   }
 
   return (
-    <section className="kino">
-      <section className="sidebar">
-        <ul>
-          <li><button onClick={() => setVideo('video/public_onboarding.mp4')}><div className="playicon"></div></button> <span>Getting Started &ndash; Your First Day at <strong>medienhaus/</strong></span></li>
-          <li><button onClick={() => setVideo('video/teacher_onboarding_visibility.mp4')}><div className="playicon"></div></button> <span>Room Visibility &ndash; Public vs. Private Rooms</span></li>
-          <li><button onClick={() => setVideo('video/teacher_onboarding_widgets.mp4')}><div className="playicon"></div></button> <span>Widgets &ndash; Integrate audio/video chat or collaborative writing</span></li>
-        </ul>
-      </section>
-      <section className="videos">
-        <div className="videowrapper">
-          <ReactPlayer className="videoplayer" playing url={video} light controls volume='0.6' width='100%' onProgress={handleProgress} />
-          <progress max={1} value={played} />
-        </div>
-      </section>
+    <section className="kino with-sidebar">
+      <div>
+        <section className="sidebar">
+          <ul>
+            <li>
+              <button onClick={() => setVideo('video/public_onboarding.mp4')}>
+                <div className="playicon"></div>
+              </button>
+              <div>
+                <p><em>01. The Getting Started Video</em></p>
+                <p>Your first day at <strong>medienhaus/</strong></p>
+              </div>
+            </li>
+            <li>
+              <button onClick={() => setVideo('video/teacher_onboarding_visibility.mp4')}>
+                <div className="playicon"></div>
+              </button>
+              <div>
+                <p><em>02. Room Visibility and Access</em></p>
+                <p>Public, private, and invite-only</p>
+              </div>
+            </li>
+            <li>
+              <button onClick={() => setVideo('video/teacher_onboarding_widgets.mp4')}>
+                <div className="playicon"></div>
+              </button>
+              <div>
+                <p><em>03. Widgets and Enhancements</em></p>
+                <p>Integrate <strong>/meet</strong>, <strong>/write</strong>, <strong>/stream</strong></p>
+              </div>
+            </li>
+          </ul>
+        </section>
+        <section className="video">
+          <div className="videowrapper">
+            <ReactPlayer className="videoplayer" playing url={video} light controls volume='0.6' width='100%' onProgress={handleProgress} />
+            <progress max={1} value={played} />
+          </div>
+        </section>
+      </div>
     </section>
   )
 }
