0f45da7210f12bb61c414b372f58baa34ac38a4a
Andi Rueckel <andirueckel@users.noreply.github.com>
2020-10-21 13:24:51 +0200

    fix some css for videos, add noopener to meet and write and stream

--- src/routes/write/index.js
+++ src/routes/write/index.js
@@ -6,7 +6,7 @@ const Write = () => {
       <p>You can use <strong>/write</strong> for taking notes and sharing them with your group in real time.</p>
       <p>Write, edit, and read collaboratively and discuss written text with by marking it and adding comments. Simply create a new pad with a random name&thinsp;&mdash;&thinsp;or manually define a name for your pad&thinsp;&mdash;&thinsp;and share your unique link with others.</p>
       <p>All your changes are backed up by a feature called timeslider, which shows all changes made to the document over time.</p>
-      <p><strong>/write</strong> can be accessed via: <a href="https://write.medienhaus.udk-berlin.de" rel="external nofollower noreferrer" target="_blank">write.medienhaus.udk-berlin.de</a></p>
+      <p><strong>/write</strong> can be accessed via: <a href="https://write.medienhaus.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">write.medienhaus.udk-berlin.de</a></p>
     </section>
   );
 }
