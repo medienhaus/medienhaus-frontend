665a8400810a708bc2fa5b386a32dfb12a042f2b
Andi Rueckel <andirueckel@udk-berlin.de>
2020-10-22 17:56:12 +0200

    add cast to /stream until server is installed

--- src/routes/stream/index.js
+++ src/routes/stream/index.js
@@ -6,7 +6,8 @@ const Stream = () => {
       <p>You can use <strong>/stream</strong> for broadcasting your video (a webcam or your desktop) and audio (your voice via microphone or your music for example) to the internet.</p>
       <p>Live streaming is different than /meet in the fact that you can stream to an unlimited amount of people who just tune in to your stream with a link.</p>
       <p>You can incorporate this into a project of yours or use it for traditional live streaming to an audience. It is up to you how to use it.</p>
-      <p><strong>/stream</strong> can be accessed via: <a href="https://stream.medienhaus.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">stream.medienhaus.udk-berlin.de</a></p>
+      <p><del><strong>/stream</strong> can be accessed via: <a href="https://stream.medienhaus.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">stream.medienhaus.udk-berlin.de</a></del></p>
+      <p>NOTE: Until we finished updating and renewing our live streaming service, you can use our testing instance via: <a href="https://cast.medienhaus.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">cast.medienhaus.udk-berlin.de</a></p>
     </section>
   );
 }
