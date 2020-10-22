4ffd9ffee3402415f38c638ed58612b632f1666a
Andi Rueckel <andirueckel@users.noreply.github.com>
2020-10-22 10:39:20 +0200

    add class for copy text, improve css, change kino from player left to player right

--- src/routes/stream/index.js
+++ src/routes/stream/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Stream = () => {
   return (
-    <section className="stream">
-      <p>We will introduce RTMP live streaming here.</p>
+    <section className="stream copy">
       <p>You can use <strong>/stream</strong> for broadcasting your video (a webcam or your desktop) and audio (your voice via microphone or your music for example) to the internet.</p>
       <p>Live streaming is different than /meet in the fact that you can stream to an unlimited amount of people who just tune in to your stream with a link.</p>
       <p>You can incorporate this into a project of yours or use it for traditional live streaming to an audience. It is up to you how to use it.</p>
