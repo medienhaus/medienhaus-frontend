c07f64c27ec37d6671ca78d7511c0dc3265868fd
Marcel <marcel-klasse@users.noreply.github.com>
2020-10-25 19:57:56 +0100

    changes

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-xhr-backend';
+import Cache from 'i18next-localstorage-cache';
 
 i18n
   // learn more: https://github.com/i18next/i18next-xhr-backend
   .use(Backend)
+  .use(Cache)
   // connect with React
   .use(initReactI18next)
   // for all options read: https://www.i18next.com/overview/configuration-options
@@ -13,6 +15,19 @@ i18n
     lng: 'en',
     fallbackLng: 'en',
     whitelist: ['en', 'de'],
+    Cache: {
+      // turn on or off
+      enabled: true,
+
+      // prefix for stored languages
+      prefix: 'i18next_res_',
+
+      // expiration
+      expirationTime: 7 * 24 * 60 * 60 * 1000,
+
+      // language versions
+      versions: {}
+    },
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
