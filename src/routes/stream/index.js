9d2095b99c354e2ad601c98aebe68db539500375
Marcel <marcel@klasse.io>
2020-10-25 09:06:49 +0100

    added more translation data

--- src/routes/stream/index.js
+++ src/routes/stream/index.js
@@ -8,7 +8,7 @@ const Stream = () => {
       <p><Trans i18nKey="stream:p0">You can use <strong>/stream</strong> for broadcasting your video (a webcam or your desktop) and audio (your voice via microphone or your music for example) to the internet.</Trans></p>
       <p>{t('stream:p1')}</p>
       <p>{t('stream:p2')}</p>
-      <p><Trans i18nKey="stream:p3"><del><strong>/stream</strong> can be accessed via: <a href="https://stream.medienhaus.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">stream.medienhaus.udk-berlin.de</a></del></Trans></p>
+      <p><del><Trans i18nKey="stream:p3"><strong>/stream</strong> can be accessed via: <a href="https://stream.medienhaus.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">stream.medienhaus.udk-berlin.de</a></Trans></del></p>
       <p><Trans i18nKey="stream:p4">NOTE: Until we finished updating and renewing our live streaming service, you can use our testing instance via: <a href="https://cast.medienhaus.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">cast.medienhaus.udk-berlin.de</a></Trans></p>
     </section>
   );
