dff5d9965af3eea9d9aae4c458984a0c3bce9828
Andi Rueckel <andirueckel@users.noreply.github.com>
2020-10-25 19:52:30 +0100

    korrektur

--- src/routes/write/index.js
+++ src/routes/write/index.js
@@ -8,7 +8,7 @@ const Write = () => {
       <p><Trans i18nKey="write:p0">You can use <strong>/write</strong> for taking notes and sharing them with your group in real time.</Trans></p>
       <p>{t('write:p1')}</p>
       <p>{t('write:p2')}</p>
-      <p><Trans i18nKey="write:p3"><strong>/write</strong> can be accessed via: <a href="https://write.medienhaus.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">write.medienhaus.udk-berlin.de</a></Trans> </p>
+      <p><Trans i18nKey="write:p3"><strong>/write</strong> can be accessed via: <a href="https://write.medienhaus.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">write.medienhaus.udk-berlin.de</a></Trans></p>
     </section>
   );
 }
