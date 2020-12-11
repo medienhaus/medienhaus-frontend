b139dc5cef002440963842b4672738f2a9a45a6f
Marcel <marcel@klasse.io>
2020-12-11 16:57:59 +0100

    Added account request to /request

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import Stream from "./routes/stream";
 import Login from "./routes/login";
 import Landing from "./routes/landing";
 import Dashboard from "./routes/dashboard";
+import Admin from "./routes/admin";
 
 const App = () => (
   <React.Fragment>
@@ -56,6 +57,7 @@ const App = () => (
                     <Route path="/meet" component={Meet} />
                     <Route path="/write" component={Write} />
                     <Route path="/stream" component={Stream} />
+                    <Route path="/admin" component={Admin} />
                   </>)
               }
             </Switch>
