51e2e6e2dda74724bb0dc230526aebece81bbe0a
Marcel <marcel-klasse@users.noreply.github.com>
2020-12-22 15:16:44 +0100

    cleaned up admin panel

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import Stream from "./routes/stream";
 import Login from "./routes/login";
 import Landing from "./routes/landing";
 import Dashboard from "./routes/dashboard";
-//import Admin from "./routes/admin";
+import Admin from "./routes/admin";
 
 const App = () => (
   <React.Fragment>
@@ -43,6 +43,7 @@ const App = () => (
                   <Route path="/meet" component={Login} />
                   <Route path="/write" component={Login} />
                   <Route path="/stream" component={Login} />
+                  <Route path="/admin" component={Admin} />
                 </>
               ) : (
                   <>
