64068363bce89c0db52c4102e10c243e8b139979
Florian Weber <florian@weber.world>
2021-03-07 23:01:40 +0100

    Bundle our language translations with the app, rather than relying on separate XHR requests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect, useLocation } from 'react-router-dom'
 import Header from './components/header'
 import Footer from './components/footer'
@@ -59,27 +59,23 @@ const App = () => (
       <Router>
         <Header />
         <main>
-          <Suspense fallback={<Loading />}>
-            <Switch>
-              <Route path="/" exact component={Landing} />
-              <Route path="/login" component={Login} />
-              <PrivateRoute path="/dashboard" component={Dashboard} />
-              <PrivateRoute path="/account" component={Account} />
-              <PrivateRoute path="/explore" component={Explore} />
-              <PrivateRoute path="/request" component={Request} />
-              <PrivateRoute path="/support" component={Support} />
-              <PrivateRoute path="/kino" component={Kino} />
-              <PrivateRoute path="/meet" component={Meet} />
-              <PrivateRoute path="/write" component={Write} />
-              <PrivateRoute path="/stream" component={Stream} />
-              <PrivateRoute path="/admin" component={Admin} />
-            </Switch>
-          </Suspense>
+          <Switch>
+            <Route path="/" exact component={Landing} />
+            <Route path="/login" component={Login} />
+            <PrivateRoute path="/dashboard" component={Dashboard} />
+            <PrivateRoute path="/account" component={Account} />
+            <PrivateRoute path="/explore" component={Explore} />
+            <PrivateRoute path="/request" component={Request} />
+            <PrivateRoute path="/support" component={Support} />
+            <PrivateRoute path="/kino" component={Kino} />
+            <PrivateRoute path="/meet" component={Meet} />
+            <PrivateRoute path="/write" component={Write} />
+            <PrivateRoute path="/stream" component={Stream} />
+            <PrivateRoute path="/admin" component={Admin} />
+          </Switch>
         </main>
         <Nav />
-        <Suspense fallback="">
-          <Footer />
-        </Suspense>
+        <Footer />
       </Router>
     </AuthProvider>
   </React.Fragment>
