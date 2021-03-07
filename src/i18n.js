a4fa5faaa80cf094e9832f214b1037732a419a01
Florian Weber <florian@weber.world>
2021-03-07 23:01:40 +0100

    Bundle our language translations with the app, rather than relying on separate XHR requests

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,10 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import Backend from 'i18next-xhr-backend'
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+const resBundle = require(
+  'i18next-resource-store-loader!./assets/locales/index.js'
+)
 
 let lng
 if (localStorage.getItem('cr_lang') !== null) {
@@ -10,12 +14,11 @@ if (localStorage.getItem('cr_lang') !== null) {
 }
 
 i18n
-  // learn more: https://github.com/i18next/i18next-xhr-backend
-  .use(Backend)
   // connect with React
   .use(initReactI18next)
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    resources: resBundle,
     debug: false,
     lng,
     nsSeparator: false,
@@ -24,6 +27,9 @@ i18n
     whitelist: ['en', 'de'],
     interpolation: {
       escapeValue: false // not needed for react as it escapes by default
+    },
+    react: {
+      useSuspense: false
     }
   })
 
