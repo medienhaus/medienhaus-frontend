8f9d4299c995a2eaed457855393cef68685b31b2
Florian Weber <fnwbr@users.noreply.github.com>
2022-07-25 13:08:37 +0200

    Update index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <p className="copyleft">&#x1f12f; <script>document.write(new Date().getFullYear());</script><noscript>202X</noscript> <a href="https://medienhaus.dev/" rel="nofollow noopener noreferrer"><strong>medienhaus/</strong></a></p>
+      <p className="copyleft">&#x1f12f; {(new Date().getFullYear())} <a href="https://medienhaus.dev/" rel="nofollow noopener noreferrer"><strong>medienhaus/</strong></a></p>
     </footer>
   )
 }
